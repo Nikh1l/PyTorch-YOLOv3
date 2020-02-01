a4790be3a94dfbaec996561e16cfd4d47e12fa22
Nikhil <nk20997@gmail.com>
2020-02-01 14:41:44 +0530

    removed duplicates

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ print(last_weight)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", type=int, default=1000, help="number of epochs")
+    parser.add_argument("--epochs", type=int, default=200, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
     parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
     parser.add_argument("--model_def", type=str, default="config/yolov3-tiny.cfg", help="path to model definition file")
